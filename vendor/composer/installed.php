48f666b9191166797da55b1c3b3b8a2f19e0c344
Balasuryadeveloper <balasuryas44210@gmail.com>
2022-01-09 13:50:40 +0530

    Composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '775b0f7d1e59e911ed11691c41ea7ea6f182533b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '775b0f7d1e59e911ed11691c41ea7ea6f182533b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
